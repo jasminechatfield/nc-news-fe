8c30031a4002fdebff435ad98ef3a6e3deda8223
Jasmine Chatfield <jazzchatfield@gmail.com>
2019-11-19 13:42:31 +0000

    added links to topics

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 export const TopicCard = props => {
   return (
     <li>
-      <h3>{props.topic.slug}</h3>
+      <h3>
+        <Link to={`/topics/${props.topic.slug}`}>{props.topic.slug}</Link>
+      </h3>
       <p>{props.topic.description}</p>
     </li>
   );
